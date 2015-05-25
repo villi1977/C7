7334a586221986b8276f81c9f955674246df6dc4
villi1977 <sergey.tokarev@hivext.net>
2015-05-25 04:26:13 -0400

    first commit4

--- vzkernel/drivers/block/ploop/ploop_events.c
+++
@@ -1,9 +0,0 @@
-#include <linux/module.h>
-#include <linux/interrupt.h>
-
-#define CREATE_TRACE_POINTS
-#include "ploop_events.h"
-
-EXPORT_TRACEPOINT_SYMBOL(submit);
-EXPORT_TRACEPOINT_SYMBOL(submit_alloc);
-EXPORT_TRACEPOINT_SYMBOL(cached_submit);
--- vzkernel_old/arch/x86/boot/voffset.h
+++
@@ -1,2 +0,0 @@
-#define VO__end 0xffffffff82637000
-#define VO__text 0xffffffff81000000
