29374fcfe79bdc25be45e8f7978403486c07aaed
villi1977 <sergey.tokarev@hivext.net>
2015-05-25 12:12:14 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 yum install -y libxml2-devel  e2fsprogs-devel libtool automake.noarch autoconf lsof wget texinfo libselinux-devel libsepol-devel libcgroup.x86_64 libcgroup-devel.x86_64 bridge-utils attr wget
 #cd /usr/local/src
-wget http://openvz.sciserv.eu/utils/e2fsprogs/1.42.11-1.ovz/src/e2fsprogs-1.42.11-1.ovz.src.rpm
+#wget http://openvz.sciserv.eu/utils/e2fsprogs/1.42.11-1.ovz/src/e2fsprogs-1.42.11-1.ovz.src.rpm
 rpm -i e2fsprogs-1.42.11-1.ovz.src.rpm
 rpmbuild -ba /root/rpmbuild/SPECS/e2fsprogs.spec
 rpm -i /root/rpmbuild/RPMS/x86_64/e2fsprogs-*
@@ -15,6 +15,7 @@ cd ../vzquota/
 make rpms
 rpm --force -Uvh /root/rpmbuild/RPMS/x86_64/ploop-*
 rpm --force --nodeps -Uvh /root/rpmbuild/RPMS/x86_64/vzquota*
+rpm --force -Uvh /root/rpmbuild/RPMS/noarch/ploop*
 cd ../vzctl
 ./autogen.sh
 make rpms
